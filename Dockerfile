90c81f02bf42d9d9b1fad4dcca4ef491d6ddf843
Leonardo <leonardo.simao1988@gmail.com>
2025-11-28 09:12:10 -0300

    Aula 2

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu:latest
 WORKDIR /app
-ENV HOST=localhost PORT=5432
-ENV USER=root PASSWORD=root DBNAME=root
+ENV DB_HOST=localhost DB_PORT=5432
+ENV DB_USER=root DB_PASSWORD=root DB_NAME=root
 COPY ./main main
 EXPOSE 8000
 CMD [ "./main" ]
--- database/db.go
+++ database/db.go
@@ -3,6 +3,7 @@ package database
 import (
 	"log"
 	"os"
+	"time"
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,9 +15,42 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	
-	stringDeConexao := "host=" + os.Getenv("HOST") + " user=" + os.Getenv("USER") + " password=" + os.Getenv("PASSWORD") + " dbname=" + os.Getenv("DBNAME") + " port=" + os.Getenv("PORT") + " sslmode=disable"
-	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+	// Use explicit DB_* env variables to avoid clobbering system vars (e.g. USER)
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	if host == "" {
+		host = "localhost"
+	}
+	if port == "" {
+		port = "5432"
+	}
+	if user == "" {
+		user = "root"
+	}
+	if password == "" {
+		password = "root"
+	}
+	if dbname == "" {
+		dbname = "root"
+	}
+
+	stringDeConexao := "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port + " sslmode=disable"
+
+	// retry logic: try for up to ~30 seconds
+	for i := 0; i < 15; i++ {
+		DB, err = gorm.Open(postgres.Open(stringDeConexao))
+		if err == nil {
+			break
+		}
+		// sleep a bit and try again
+		// Use time.Sleep without import collision
+		// import time
+		time.Sleep(2 * time.Second)
+	}
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
 	}
